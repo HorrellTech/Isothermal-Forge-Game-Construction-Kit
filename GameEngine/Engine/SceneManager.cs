e035539e68f4801d8a5f3e946d04be5ac8427476
HorrellTech <horrelltechnologies@gmail.com>
2018-08-07 17:42:18 +1200

    Add SceneManager class

---
+++ GameEngine/Engine/SceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Engine
+{
+    class SceneManager
+    {
+        public List<Scene> scenes = new List<Scene>(); // Hold a list of the scenes in the game
+
+        public SceneManager()
+        {
+
+        }
+    }
+}

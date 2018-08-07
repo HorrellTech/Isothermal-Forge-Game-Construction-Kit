8297a348305e19cbcf31fc01e3edfe81c0286b41
HorrellTech <horrelltechnologies@gmail.com>
2018-08-07 17:31:56 +1200

    Add delegates for events to GameObject

--- GameEngine/Engine/GameObject.cs
+++ GameEngine/Engine/GameObject.cs
@@ -8,11 +8,21 @@ namespace GameEngine.Engine
 {
     class GameObject
     {
+        // Delegates and events for 
+        public delegate void AwakeEvent();
+        public delegate void UpdateEvent();
+        public delegate void UpdateBeginEvent();
+        public delegate void UpdateEndEvent();
+        public delegate void DrawEvent();
 
+        public event AwakeEvent OnAwake;
+        public event UpdateEvent OnUpdate;
+        public event UpdateBeginEvent OnUpdateBegin;
+        public event UpdateEndEvent OnUpdateEnd;
+        public event DrawEvent OnDraw;
 
         public GameObject()
         {
-
         }
     }
 }

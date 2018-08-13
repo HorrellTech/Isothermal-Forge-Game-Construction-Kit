6cea53320f47eded7ea3f640526cf71e1ec8f396
HorrellTech <horrelltechnologies@gmail.com>
2018-08-13 21:19:16 +1200

    Change some minor stuff

--- Editor/Form1.Designer.cs
+++ Editor/Form1.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.darkMenuStrip1 = new DarkUI.Controls.DarkMenuStrip();
-            this.darkToolStrip1 = new DarkUI.Controls.DarkToolStrip();
-            this.darkStatusStrip1 = new DarkUI.Controls.DarkStatusStrip();
-            this.darkDockPanel1 = new DarkUI.Docking.DarkDockPanel();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.projectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.darkToolStrip1 = new DarkUI.Controls.DarkToolStrip();
+            this.darkStatusStrip1 = new DarkUI.Controls.DarkStatusStrip();
+            this.dockPanel = new DarkUI.Docking.DarkDockPanel();
             this.darkMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,43 +57,6 @@
             this.darkMenuStrip1.Size = new System.Drawing.Size(1163, 28);
             this.darkMenuStrip1.TabIndex = 0;
             this.darkMenuStrip1.Text = "darkMenuStrip1";
-            // 
-            // darkToolStrip1
-            // 
-            this.darkToolStrip1.AutoSize = false;
-            this.darkToolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
-            this.darkToolStrip1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
-            this.darkToolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
-            this.darkToolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.darkToolStrip1.Location = new System.Drawing.Point(0, 28);
-            this.darkToolStrip1.Name = "darkToolStrip1";
-            this.darkToolStrip1.Padding = new System.Windows.Forms.Padding(5, 0, 1, 0);
-            this.darkToolStrip1.Size = new System.Drawing.Size(1163, 28);
-            this.darkToolStrip1.TabIndex = 1;
-            this.darkToolStrip1.Text = "darkToolStrip1";
-            // 
-            // darkStatusStrip1
-            // 
-            this.darkStatusStrip1.AutoSize = false;
-            this.darkStatusStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
-            this.darkStatusStrip1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
-            this.darkStatusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.darkStatusStrip1.Location = new System.Drawing.Point(0, 657);
-            this.darkStatusStrip1.Name = "darkStatusStrip1";
-            this.darkStatusStrip1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 3);
-            this.darkStatusStrip1.Size = new System.Drawing.Size(1163, 32);
-            this.darkStatusStrip1.SizingGrip = false;
-            this.darkStatusStrip1.TabIndex = 2;
-            this.darkStatusStrip1.Text = "darkStatusStrip1";
-            // 
-            // darkDockPanel1
-            // 
-            this.darkDockPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
-            this.darkDockPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.darkDockPanel1.Location = new System.Drawing.Point(0, 56);
-            this.darkDockPanel1.Name = "darkDockPanel1";
-            this.darkDockPanel1.Size = new System.Drawing.Size(1163, 601);
-            this.darkDockPanel1.TabIndex = 3;
             // 
             // fileToolStripMenuItem
             // 
@@ -130,12 +93,49 @@
             this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
             this.helpToolStripMenuItem.Text = "Help";
             // 
+            // darkToolStrip1
+            // 
+            this.darkToolStrip1.AutoSize = false;
+            this.darkToolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.darkToolStrip1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkToolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.darkToolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.darkToolStrip1.Location = new System.Drawing.Point(0, 28);
+            this.darkToolStrip1.Name = "darkToolStrip1";
+            this.darkToolStrip1.Padding = new System.Windows.Forms.Padding(5, 0, 1, 0);
+            this.darkToolStrip1.Size = new System.Drawing.Size(1163, 28);
+            this.darkToolStrip1.TabIndex = 1;
+            this.darkToolStrip1.Text = "darkToolStrip1";
+            // 
+            // darkStatusStrip1
+            // 
+            this.darkStatusStrip1.AutoSize = false;
+            this.darkStatusStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.darkStatusStrip1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkStatusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.darkStatusStrip1.Location = new System.Drawing.Point(0, 657);
+            this.darkStatusStrip1.Name = "darkStatusStrip1";
+            this.darkStatusStrip1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 3);
+            this.darkStatusStrip1.Size = new System.Drawing.Size(1163, 32);
+            this.darkStatusStrip1.SizingGrip = false;
+            this.darkStatusStrip1.TabIndex = 2;
+            this.darkStatusStrip1.Text = "darkStatusStrip1";
+            // 
+            // dockPanel
+            // 
+            this.dockPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.dockPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dockPanel.Location = new System.Drawing.Point(0, 56);
+            this.dockPanel.Name = "dockPanel";
+            this.dockPanel.Size = new System.Drawing.Size(1163, 601);
+            this.dockPanel.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1163, 689);
-            this.Controls.Add(this.darkDockPanel1);
+            this.Controls.Add(this.dockPanel);
             this.Controls.Add(this.darkStatusStrip1);
             this.Controls.Add(this.darkToolStrip1);
             this.Controls.Add(this.darkMenuStrip1);
@@ -155,7 +155,7 @@
         private DarkUI.Controls.DarkMenuStrip darkMenuStrip1;
         private DarkUI.Controls.DarkToolStrip darkToolStrip1;
         private DarkUI.Controls.DarkStatusStrip darkStatusStrip1;
-        private DarkUI.Docking.DarkDockPanel darkDockPanel1;
+        private DarkUI.Docking.DarkDockPanel dockPanel;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;

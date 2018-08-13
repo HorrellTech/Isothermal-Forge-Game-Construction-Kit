d3c89e5e51270249ad41340b650563a1cbddd5b5
HorrellTech <horrelltechnologies@gmail.com>
2018-08-13 21:19:56 +1200

    Add RGB color value returning

---
+++ GameEngine/js/drawing.js
@@ -0,0 +1,9 @@
+
+// Get color from rgb color values
+function rgb(r, g, b)
+{
+    r = Math.floor(r);
+    g = Math.floor(g);
+    b = Math.floor(b);
+    return ["rgb(",r,",",g,",",b,")"].join("");
+}

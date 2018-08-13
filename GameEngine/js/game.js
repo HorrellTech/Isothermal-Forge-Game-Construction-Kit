f912317452947629ec094ca09e0de0768e5736d1
HorrellTech <horrelltechnologies@gmail.com>
2018-08-14 09:49:31 +1200

    Add math functions

--- GameEngine/js/game.js
+++ GameEngine/js/game.js
@@ -1,21 +1,45 @@
+// CONSTANTS
+const pi = Math.PI;
+
 var gameObjects = []; // The game object list
-var p;
+var context;
+var dt;
+var room_width;
+var room_height;
 
 // Start the game
 function gameStart()
 {
+    //dt = (Date.now - Date.last) / (1000 / 120);
+    game.start(640, 480);
+
+    context = game.canvas.getContext('2d');
     // Test game object
     var obj = object_add();
+    obj.width = 32;
+    obj.height = 32;
 
     obj.draw = function()
     {
-        game.context.fillStyle = "red";
-        game.context.fillRect(x, y, width, height);
+        this.x += 1;
+        if(this.x > room_width)
+        {
+            this.x = -31;
+        }
+        context.fillStyle = 'red';
+        for(var i = this.width; i > 0; i -= 1)
+        {
+            context.fillStyle = rgb(255, (i * 10), (i * 10));
+            context.fillRect(this.x - i, this.y + (i / 2), this.width - i, this.height - i);
+        }
     };
 
-    var inst = instance_create(64, 64, obj);
-
-    game.start(640, 480);
+    instance_create(64, 64, obj);
+    instance_create(64 + 32, 64 + 32, obj);
+    instance_create(64, 64 + 32 + 32, obj);
+    instance_create(64 + 32, 64 + 32 + 32 + 32, obj);
+    instance_create(64 + 32 + 32, 64 + 32 + 32 + 32 + 32, obj);
+    instance_create(64 + 32, 64 + 32 + 32 + 32 + 32 + 32, obj);
 }
 
 // The main game area where the canvas will be held
@@ -25,10 +49,12 @@ var game =
     start : function(width, height) {
         this.canvas.width = width;
         this.canvas.height = height;
+        room_width = width;
+        room_height = height;
         this.context = this.canvas.getContext('2d');
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 20);
+        this.interval = setInterval(updateGameArea, 1000 / 60);
         },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -60,6 +86,10 @@ function gameObject(x, y, width, height)
 
     this.x = x;
     this.y = y;
+    this.xstart = x;
+    this.ystart = y;
+    this.xprevious = x;
+    this.yprevious = y;
     this.width = width;
     this.height = height;
     this.hspeed = 0;
@@ -77,24 +107,58 @@ function gameObject(x, y, width, height)
     {};
 
     // Perform on every loop
-    this.update = function() 
+    this.loop = function() 
+    {};
+
+    // Perform before every loop
+    this.loop_begin = function() 
+    {};
+
+    // Perform after every loop
+    this.loop_end = function() 
+    {};
+
+    // Draw on every loop
+    this.draw = function()
+    {};
+
+    // The update called in the game update method (DO NOT OVER WRITE)
+    this.update = function()
     {
         if(!this.hasWoken)
         {
             this.awake();
             this.hasWoken = true;
+            this.xstart = this.x;
+            this.ystart = this.y;
         }
-    };
+        this.xprevious = this.x;
+        this.yprevious = this.y;
 
-    // Draw on every loop
-    this.draw = function()
+        this.loop_begin();
+        this.loop();
+
+        if(this.speed != 0)
+        {
+            this.hspeed = lengt
+        }
+
+        this.loop_end();
+    }
+
+    // The draw called in the game update method (DO NOT OVER WRITE)
+    this.mainDraw = function()
     {
-    };
+        this.draw();
+    }
 
     // Add a new instance to this object
     this.instantiate = function(x, y)
     {
-        var temp = new gameObject(x, y, width, height);
+        var temp = new gameObject(x, y, this.width, this.height);
+        temp.awake = this.awake;
+        temp.update = this.update;
+        temp.draw = this.draw;
 
         this.instances.push(temp);
 
@@ -124,29 +188,27 @@ function gameObject(x, y, width, height)
 // Main update loop
 function updateGameArea()
 {
-    game.clear();
-    game.frameNo += 1;
-    if (game.frameNo == 1 || everyinterval(150)) 
+    if(context != game.context)
     {
+        context = game.context;
+    }
+
+    game.clear();
         // This is the main game loop
         for (var i = 0; i < this.gameObjects.length; i += 1) 
         {
             for(var j = 0; j < this.gameObjects[i].instances.length; j += 1)
             {
                 this.gameObjects[i].instances[j].update();
-            //alert("" + this.gameObjects[i].instances.length);
             }
         }
         for (var x = 0; x < this.gameObjects.length; x += 1) 
         {
-            for(var y = 0; y < this.gameObjects[i].instances.length; y += 1)
+            for(var y = 0; y < this.gameObjects[x].instances.length; y += 1)
             {
-                this.gameObjects[x].instances[y].draw();
+                this.gameObjects[x].instances[y].mainDraw();
             }
         }
-        
-        //alert("" + gameObjects.length + " instances in the scene");
-    }
 }
 
 // If the canvas exists, use it, otherwise create a new one
@@ -175,4 +237,82 @@ function everyinterval(n)
         return true;
     }
     return false;
+}
+
+
+// DRAWING STUFF
+
+// Get color from rgb color values
+function rgb(r, g, b)
+{
+    r = Math.floor(r);
+    g = Math.floor(g);
+    b = Math.floor(b);
+    return ["rgb(",r,",",g,",",b,")"].join("");
+}
+
+
+// MATH STUFF
+
+// Returns the greatest integer less than or equal to its numeric argument
+function floor(x)
+{
+    return (Math.floor(x));
+}
+
+// Returns the smallest integer greater than or equal to its numeric argument
+function ceil(x)
+{
+    return (Math.ceil(x));
+}
+
+// Returns a supplied numeric expression rounded to the nearest number
+function round(x)
+{
+    return (Math.round(x));
+}
+
+// Returns the sine of a number
+function sin(x)
+{
+    return (Math.sin(x));
+}
+
+// Returns the cosine of a number
+function cos(x)
+{
+    return (Math.cos(x));
+}
+
+// Returns the cosine of a number from degrees to radians
+function dcos(x)
+{
+    return(cos(degtorad(x)));
+}
+
+// Returns degrees to radians
+function degtorad(x)
+{
+    return ((Math.PI * 2) / 360);
+}
+
+// Snap a position to a grid position
+function snap(position, grid_size)
+{
+    return (floor(position / grid_size) * grid_size);
+}
+
+// Return the distance between 2 points
+function point_distance(x1, y1, x2, y2)
+{
+    return (Math.sqrt(Math.pow(x2 - x2, 2) + Math.pow(y2 - y1, 2)));
+}
+
+// Return the direction from one point to another
+function point_direction(x1, y1, x2, y2)
+{
+    var xdiff = x2 - x1;
+    var ydiff = y2 - y1;
+
+    return(-(Math.atan2(ydiff, xdiff) * 180.0 / Math.PI));
 }

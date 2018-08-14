cb897c2bd1fcbe4277326e55507855e8ff92df50
HorrellTech <horrelltechnologies@gmail.com>
2018-08-14 17:41:36 +1200

    Removed

--- GameEngine/js/drawing.js
+++
@@ -1,9 +0,0 @@
-
-// Get color from rgb color values
-function rgb(r, g, b)
-{
-    r = Math.floor(r);
-    g = Math.floor(g);
-    b = Math.floor(b);
-    return ["rgb(",r,",",g,",",b,")"].join("");
-}

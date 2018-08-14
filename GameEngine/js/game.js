18ca736d4287959ffeb53ee4a919256ae71974d4
HorrellTech <horrelltechnologies@gmail.com>
2018-08-14 17:42:30 +1200

    Add more functions, constants, global variables, and depth rearranging

--- GameEngine/js/game.js
+++ GameEngine/js/game.js
@@ -1,32 +1,51 @@
 // CONSTANTS
-const pi = Math.PI;
+const pi = Math.PI; // PI
+const t = 1; // True
+const f = 0; // False
+const self = -1; // The instance which is executing the current block of code
+const other = -2; // The other instance involved in a collision event, or the other instance from a with function
+const all = -3; // All instances currently active in the room
+const noone = -4; // No instance at all
 
 var gameObjects = []; // The game object list
 var context;
 var dt;
+
+// GLOBAL VARIABLES
+var room_speed;
 var room_width;
 var room_height;
+var view_xview;
+var view_yview;
+var view_wview;
+var view_hview;
+var score;
+var health;
+var lives;
 
 // Start the game
 function gameStart()
 {
-    //dt = (Date.now - Date.last) / (1000 / 120);
-    game.start(640, 480);
+    room_speed = 60;
+    game.start(640, 480, '2d');
+    score = 0;
+    health = 100;
+    lives = 3;
 
-    context = game.canvas.getContext('2d');
+    context = game.context;
     // Test game object
     var obj = object_add();
     obj.width = 32;
     obj.height = 32;
+    
+    var obj2 = object_add();
+    obj2.width = 32;
+    obj2.height = 32;
 
     obj.draw = function()
     {
-        //this.hspeed = 1;
-        //this.gravity = 2;
-        //this.friction = 0.3;
-        //this.motion_add(direction, 1);
-        this.speed = random_range(0, 10);
-        this.direction += random(180);
+        this.motion_set(this.direction + random_range(-10, 10), random_range(10, 10));
+        
         if(this.x > room_width)
         {
             this.x = -31;
@@ -43,35 +62,75 @@ function gameStart()
         {
             this.y = room_height;
         }
-        context.fillStyle = 'red';
         context.fillStyle = rgb(255, 0, 0);
         context.fillRect(this.x, this.y, this.width, this.height);
     };
 
-    instance_create(64, 64, obj);
-    instance_create(64 + 32, 64 + 32, obj);
-    instance_create(64, 64 + 32 + 32, obj);
-    instance_create(64 + 32, 64 + 32 + 32 + 32, obj);
-    instance_create(64 + 32 + 32, 64 + 32 + 32 + 32 + 32, obj);
-    instance_create(64 + 32, 64 + 32 + 32 + 32 + 32 + 32, obj);
+    obj2.draw = function()
+    {
+        this.motion_set(this.direction + random_range(-10, 10), random_range(0, 3));
+        this.depth = 1;
+        
+        if(this.x > room_width)
+        {
+            this.x = -31;
+        }
+        if(this.x + this.width < 0)
+        {
+            this.x = room_width;
+        }
+        if(this.y > room_height)
+        {
+            this.y = -31;
+        }
+        if(this.y + this.height < 0)
+        {
+            this.y = room_height;
+        }
+        context.fillStyle = rgb(0, 255, 0);
+        context.fillRect(this.x - 16, this.y - 16, this.width, this.height);
+    };
+
+    for(var i = 0; i < 10; i += 1)
+    {
+        instance_create(random(room_width), random(room_height), obj);
+    }
+
+    for(var i = 0; i < 100; i += 1)
+    {
+        instance_create(random(room_width), random(room_height), obj2);
+    }
 }
 
 // The main game area where the canvas will be held
 var game = 
 {
+    cont : '2d', // The context
     canvas : createCanvas(),
-    start : function(width, height) {
+    start : function(width, height, cont) {
         this.canvas.width = width;
         this.canvas.height = height;
         room_width = width;
         room_height = height;
-        this.context = this.canvas.getContext('2d');
+        this.cont = cont;
+        this.context = this.canvas.getContext(cont);
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 1000 / 60);
+        this.interval = setInterval(updateGameArea, (1000 / room_speed));
         },
     clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        if(this.cont == '2d')
+        {
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        } 
+        else if(this.cont == 'webgl')
+        {
+            this.context.clearColor(1.0, 1.0, 1.0, 1.0);
+            this.context.clearDepth(1.0);
+            this.context.enable(this.context.DEPTH_TEST);
+            this.context.depthFunc(this.context.LEQUAL);
+            this.context.clear(this.context.COLOR_BUFFER_BIT | this.context.DEPTH_BUFFER_BIT);
+        }
     }
 };
 
@@ -97,8 +156,14 @@ function object_add()
 function gameObject(x, y, width, height) 
 {
     this.instances = [];
+    this.object_id = noone;
+    this.id = 0;
+    this.need_removed = false;
+    this.has_sorted_depth = true;
+    this.need_sorted = false;
 
     this.active = true;
+    this.visible = true;
 
     this.x = x;
     this.y = y;
@@ -106,6 +171,11 @@ function gameObject(x, y, width, height)
     this.ystart = y;
     this.xprevious = x;
     this.yprevious = y;
+    this.depth = 0;
+    this.bbox_left = x;
+    this.bbox_top = y;
+    this.bbox_right = x + width;
+    this.bbox_bottom = y + height;
     this.width = width;
     this.height = height;
     this.hspeed = 0;
@@ -148,6 +218,8 @@ function gameObject(x, y, width, height)
             this.xstart = this.x;
             this.ystart = this.y;
         }
+        var oldDepth = this.depth;
+
         this.xprevious = this.x;
         this.yprevious = this.y;
 
@@ -216,7 +288,44 @@ function gameObject(x, y, width, height)
                 this.vspeed = 0;
             }
         }
+
+        // Rearrange based on new depth
+        if(this.depth != oldDepth)
+        {
+            //object_id.has_sorted_depth = false;
+            object_id.need_sorted = true;
+        }
+
+        // If the object hasn't sorted by depth
+        if(this.need_sorted)
+        {
+            this.sort_by_depth();
+        }
     };
+
+    // Sort the instances based on their depth
+    this.sort_by_depth = function()
+    {
+        var len = this.instances.length;
+
+        for(var i = len - 1; i >= 0; i -= 1) // Loop through instances
+        {
+            for(var j = 1; j <= i; j += 1)
+            {
+                var d1 = this.instances[j].depth;
+                var d2 = this.instances[j - 1].depth;
+                if(d2 < d1)
+                {
+                    var temp2 = this.instances[j - 1];
+                    this.instances[j - 1] = this.instances[j];
+                    this.instances[j] = temp2;
+                }
+            }
+        }
+
+        this.has_sorted_depth = true;
+        this.need_sorted = false;
+    }
 
     // The draw called in the game update method (DO NOT OVER WRITE)
     this.mainDraw = function()
@@ -228,20 +337,49 @@ function gameObject(x, y, width, height)
     this.instantiate = function(x, y)
     {
         var temp = new gameObject(x, y, this.width, this.height);
+        temp.hasWoken = false;
         temp.awake = this.awake;
         temp.update = this.update;
         temp.draw = this.draw;
+        temp.object_id = this;
+        temp.id = this.id + 1;
 
         this.instances.push(temp);
+
+        this.id += 1;
 
         return(temp);
     };
 
-    // Add motion towards a direction
-    this.motion_add = function(dir, spd)
+    // Check if there are inactive instaces waiting to be removed, and if so, remove them
+    this.clean_up_instances = function()
     {
-        this.speed += spd;
-        this.direction = dir;
+        for(var i = 0; i < instances.length; i += 1)
+        {
+            var ins = this.instances[i];
+            if(!ins.active)
+            {
+                if(ins.need_removed)
+                {
+                    this.instances.splice(i, 1);
+                }
+            }
+        }
+    };
+
+    // Remove this instance from the object_id instances list
+    this.instance_destroy = function()
+    {
+        this.active = false;
+        this.need_removed = true;
+        object_id.clean_up_instances();
+    };
+
+    // Add motion towards a direction
+    this.motion_add = function(direction, speed)
+    {
+        this.speed += speed;
+        this.direction = direction;
     };
 
     // Set motion in a direction
@@ -251,42 +389,69 @@ function gameObject(x, y, width, height)
         this.direction = direction;
     };
 
-    // Collides with object of a type
-    this.collision = function(other)
+    // Move the object towards a point at a speed
+    this.move_towards_point = function(x, y, speed)
     {
-        for(var i = 0; i < other.instances.length; i += 1)
-        {
-            if(this.collides_with(other.instances[i]))
-            {
-                return (true);
-            }
-        }
-        return (false);
+        var pos = point_direction(this.x, this.y, x, y);
+        this.motion_set(pos, speed);
     }
 
-    // Collision with another game object
-    this.collides_with = function(other) 
+    // Snap the object to a grid
+    this.move_snap = function(hsnap, vsnap)
     {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = other.x;
-        var otherright = other.x + (other.width);
-        var othertop = other.y;
-        var otherbottom = other.y + (other.height);
-        var crash = true;
-        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) 
-        {
-            crash = false;
-        }
-        return crash;
+        this.x = snap(this.x, hsnap);
+        this.y = snap(this.y, vsnap);
     };
+}
+
+// A sprite object
+function sprite()
+{
+
+}
+
+// Sort all of the objects based on their top instances depth
+function sortObjectsByDepth()
+{
+    if(objectHasSortedDepth())
+    {
+    var len = this.gameObjects.length;
+
+        for(var i = len - 1; i >= 0; i -= 1) // Loop through instances
+        {
+            for(var j = 1; j <= i; j += 1)
+            {
+                var d1 = this.gameObjects[j].instances[0].depth;
+                var d2 = this.gameObjects[j - 1].instances[0].depth;
+                if(d2 < d1)
+                {
+                    var temp2 = this.gameObjects[j - 1];
+                    this.gameObjects[j - 1] = this.gameObjects[j];
+                    this.gameObjects[j] = temp2;
+                }
+            }
+        }
+    }
+}
+
+// Check if one of the objects has sorted by depth
+function objectHasSortedDepth()
+{
+    for(var i = 0; i < gameObjects.length; i += 1)
+    {
+        if(gameObjects[i].has_sorted_depth)
+        {
+            gameObjects[i].has_sorted_depth = false;
+            return (true);
+        }
+    }
+    return (false);
 }
 
 // Main update loop
 function updateGameArea()
 {
+    sortObjectsByDepth();
     if(context != game.context)
     {
         context = game.context;
@@ -298,17 +463,22 @@ function updateGameArea()
         {
             for(var j = 0; j < this.gameObjects[i].instances.length; j += 1)
             {
-                this.gameObjects[i].instances[j].update();
+                var ins = this.gameObjects[i].instances[j];
+                if(ins != null && ins.active)
+                {
+                    ins.update();
+                }
             }
         }
         for (var x = 0; x < this.gameObjects.length; x += 1) 
         {
             for(var y = 0; y < this.gameObjects[x].instances.length; y += 1)
             {
-                //if(this.gameObjects[x].instances[y].active)
-                //{
-                    this.gameObjects[x].instances[y].mainDraw();
-                //}
+                var ins = this.gameObjects[x].instances[y];
+                if(ins != null && ins.visible && ins.active)
+                {
+                    ins.mainDraw();
+                }
             }
         }
 }
@@ -393,15 +563,27 @@ function cos(x)
 }
 
 // Returns the cosine of a number from degrees to radians
-/*function dcos(x)
+function dcos(x)
 {
     return(cos(degtorad(x)));
-}*/
+}
 
 // Returns degrees to radians
-function degtorad()
+function degtorad2()
 {
-    return ((Math.PI * 2) / -360);
+    return ((pi * 2) / -360);
+}
+
+// Returns degrees to radians
+function degtorad(x)
+{
+    return (x * pi / 180);
+}
+
+// Returns radians to degrees
+function radtodeg(x)
+{
+    return (x * 180 / pi);
 }
 
 // Snap a position to a grid position
@@ -428,13 +610,13 @@ function point_direction(x1, y1, x2, y2)
 // Returns the length and direction on the x axis
 function lengthdir_x(length, direction)
 {
-    return (length * cos(direction * degtorad()));
+    return (length * cos(direction * degtorad2()));
 }
 
 // Returns the length and direction on the y axis
 function lengthdir_y(length, direction)
 {
-    return (length * sin(direction * degtorad()));
+    return (length * sin(direction * degtorad2()));
 }
 
 // Lerp a value towards another value
@@ -483,4 +665,13 @@ function string(val)
 function real(val)
 {
     return (parseInt(val));
+}
+
+// Execute javascript code from a string
+function execute_string(string)
+{
+    var script = document.createElement("script");
+    script.onload = script.onerror = function(){ this.remove(); };
+    script.src = "data:text/plain;base64," + btoa(source);
+    document.body.appendChild(script);
 }

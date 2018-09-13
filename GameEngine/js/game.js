824c0a98067bf86c31cb0440885b87f5dd6747e1
Joseph Horrell <horrelltechnologies@gmail.com>
2018-09-14 09:02:52 +1200

    Add create_array

--- GameEngine/js/game.js
+++ GameEngine/js/game.js
@@ -1725,6 +1725,19 @@ function ray_info()
 
 // MATH STUFF
 
+// Create an array
+function create_array(length) {
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}
+
 // Returns the greatest integer less than or equal to its numeric argument
 function floor(x)
 {

367fefb38892ae1cf65bb93f5fde347d25ddce54
Joseph Horrell <horrelltechnologies@gmail.com>
2018-09-21 11:11:58 +1200

    Add create 2d array function

--- GameEngine/js/game.js
+++ GameEngine/js/game.js
@@ -1726,13 +1726,17 @@ function ray_info()
 // MATH STUFF
 
 // Create an array
-function create_array(length) {
-    var arr = new Array(length || 0),
-        i = length;
+function array_create_2d(width, height, defaultValue) 
+{
+    var arr = [];
 
-    if (arguments.length > 1) {
-        var args = Array.prototype.slice.call(arguments, 1);
-        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    for(var i = 0; i < width; i += 1)
+    {
+        for(var j = 0; j < height; j += 1)
+        {
+            arr.push([i, j]);
+            arr[i][j] = defaultValue;
+        }
     }
 
     return arr;
